b54e6d5e303c8936f5005543c7291c9eb2bc47f3
eitanschreiber123 <eitanschreiber97@gmail.com>
2017-09-19 13:54:02 -0400

    Update new-feature.ts

--- src/component/job/new-feature.ts
+++ src/component/job/new-feature.ts
@@ -9,32 +9,9 @@ export class newFeatures {
 public features: [string];
 
 constructor(private http: HttpClient) {
-  this.updateJobs()
   this.features = [''];
 }
-updateJobs() {
-  this.http.post('http://localhost:3000/jobs', {feature: this.features}).subscribe((data: any) => {
-
-  if (data.success) {
-      this.features = data.features;
-    }
-        console.log('api data', data);
-  }, (err) => {
-    console.log('api error', err);
-  });
-}
 postFeature(n) {
-  this.updateJobs()
   this.features.push(n);
-}
-refresh() {
-  this.http.get('http://localhost:3000/jobs').subscribe((data: any) => {
-  if (data.success) {
-      this.features = data.features;
-    }
-        console.log('api data', data);
-  }, (err) => {
-    console.log('api error', err);
-  });
 }
 }
